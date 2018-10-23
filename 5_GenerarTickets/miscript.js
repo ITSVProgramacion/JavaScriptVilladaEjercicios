47c2d28b057af16571a5f846d48376ec0702a27d
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2018-10-23 08:38:04 -0300

    Correcciones

--- 5_GenerarTickets/miscript.js
+++ 5_GenerarTickets/miscript.js
@@ -14,8 +14,17 @@ document.getElementById("purchaseBtn").onclick = function(){
 * Función que genera una colección de datos a partir de un formulario.
 **/
 function generarJSONDesdeFormulario() {
+
+  var obj = {"name": document.getElementById("name").value,
+            "lastname": document.getElementById("lastname").value,
+            "dni": document.getElementById("dni").value,
+            "seat": document.getElementById("seat").value,
+            "rowSelector": document.getElementById("rowSelector").value,
+            "sectorSelector": document.getElementById("sectorSelector").value };
+
+  //** OTRA FORMA DE HACER ESTO, ES DE ESTA MANERA:
   //Declaro obj es el JSON que voy a devolver y elements, obtiene todos los datos del formulario html.
-  var obj = {};
+  /*var obj = {};
   var elements = form.querySelectorAll("input, select, textarea");
 
   //Seteo cada elemento a obj con la estructura "nombre:valor,".
@@ -24,8 +33,10 @@ function generarJSONDesdeFormulario() {
     if (element.name) {
       obj[element.name] = element.value;
     }
-  }
-  //Se convierte obj en JSON y lo devuelvo.
+  }*/
+  //**
+
+  //Devuelvo el objeto como String.
   return JSON.stringify(obj);
 }
 
@@ -33,17 +44,19 @@ function generarJSONDesdeFormulario() {
 * Función que imprime en un DIV los tickets que se van generando.
 **/
 function printTicket() {
+  //Controlo el numero de ticket a impirmir.
   if (numTicket == 0) {
     numTicket = 1;
   } else {
     numTicket++;
   }
 
+  //Obtengo el objeto JSON de los datos del comprardor.
   var data = JSON.parse(ticketsArray[numTicket-1]);
-  var divContent = "";
 
-  divContent += '<div id="ticket" class="ticket">';
-  divContent += '<label name="ticketNumber">N° ' + numTicket + '</label></br></br>';
+  //Genero el div con el nuevo ticket a imprimit.
+  var divContent += '<div id="ticket" class="ticket">';
+  divContent += '<label name="ticketNumber">N ' + numTicket + '</label></br></br>';
   divContent += '<div name="eventTitle" class="eventTitle">'+ dataEvent.eventName + '</div></br>';
   divContent += '<p name="eventDate" class="alignRight">' + dataEvent.eventDate + ' - ' + dataEvent.eventTime + '</p></br>';
   divContent += '<p name="personName">Nombre: ' + data.name + ' ' + data.lastname + '</p><p name="DNI">DNI: ' + data.dni + '</p>';
@@ -51,5 +64,6 @@ function printTicket() {
   divContent += '<div class="column seatRawSectorPrice" name="dataSeatRawSector">' + data.seat + ' - ' + data.rowSelector + ' - ' + data.sectorSelector + '</div><div class="column seatRawSectorPrice alignRight" name="price">$' + dataEvent.eventPrice + '.-</div>';
   divContent += '</div></div>';
 
+  //Agrego el nuevo ticket al html
   document.getElementById('ticketPrinter').innerHTML += divContent;
 }
