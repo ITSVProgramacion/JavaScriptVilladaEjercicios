eeacba14110423961173bf1f1c7de5b4ad921945
nicolas.fanin <nicolas.x.fanin.-nd@disney.com>
2018-11-06 08:55:42 -0300

    Ejercicio AJAX

--- 5_GenerarTickets/miscript.js
+++ 5_GenerarTickets/miscript.js
@@ -1,5 +1,8 @@
 //Defino las variables que voy a utilizar.
-var dataEvent = { eventName : "Super Evento Fantastico!", eventPrice : 2000, eventDate : "2018-11-10",  eventTime : "09:00:00"};
+var dataEvent = { eventName : "Super Evento Fantastico!",
+                eventPrice : 2000,
+                eventDate : "2018-11-10",
+                eventTime : "09:00:00"};
 var ticketsArray = [];
 var numTicket = 0;
 
@@ -55,7 +58,7 @@ function printTicket() {
   var data = JSON.parse(ticketsArray[numTicket-1]);
 
   //Genero el div con el nuevo ticket a imprimit.
-  var divContent += '<div id="ticket" class="ticket">';
+  var divContent = '<div id="ticket" class="ticket">';
   divContent += '<label name="ticketNumber">N ' + numTicket + '</label></br></br>';
   divContent += '<div name="eventTitle" class="eventTitle">'+ dataEvent.eventName + '</div></br>';
   divContent += '<p name="eventDate" class="alignRight">' + dataEvent.eventDate + ' - ' + dataEvent.eventTime + '</p></br>';
---
+++ AJAX/miAjaxScript.js
@@ -0,0 +1,18 @@
+var xhttp = new XMLHttpRequest();
+
+document.getElementById("obtenerInfoBtn").onclick = function(){
+
+  xhttp.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+        var obj = JSON.parse(this.responseText);
+        console.log(obj);
+        console.log(this.responseText);
+        //console.log(obj.data.eventTime);
+        document.getElementById("content").innerHTML = obj.data.eventName;
+      }
+  };
+
+  xhttp.open('GET', 'http://localhost/ajax/?q=data.json', true);
+  xhttp.send();
+
+};
