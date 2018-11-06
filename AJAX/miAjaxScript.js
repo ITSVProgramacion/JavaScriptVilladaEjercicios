f396d9c7c185d870542989448a8f0d3912fc8b5b
Francisco de Maussion <franciscodemaussion@gmail.com>
2018-11-06 10:16:38 -0300

    Update miAjaxScript.js

--- AJAX/miAjaxScript.js
+++ AJAX/miAjaxScript.js
@@ -12,7 +12,7 @@ document.getElementById("obtenerInfoBtn").onclick = function(){
       }
   };
 
-  xhttp.open('GET', 'https://nikosff.github.io/JavaScriptVilladaEjercicios/AJAX/?q=data.json', true);
+  xhttp.open('GET', 'https://nikosff.github.io/JavaScriptVilladaEjercicios/AJAX/data.json', true);
   xhttp.send();
 
 };
